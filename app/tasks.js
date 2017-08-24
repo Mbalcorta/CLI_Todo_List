12050fe33dd48806ef2f178acca037f8dc538193
Mateo Balcorta <misterrattail@gmail.com>
2017-08-24 13:03:02 -0700

    makes json file when one does not exist

--- app/tasks.js
+++ app/tasks.js
@@ -1,10 +1,21 @@
 #!/usr/bin/env node
+const path = require('path');
+const fs = require('fs');
 const add = require('./commands/add.js').add;
 const complete = require('./commands/complete.js').complete;
 const deleted = require('./commands/delete.js').deleted;
 const list = require('./commands/list.js').list;
 const args = process.argv.slice(2);
 const firstArgument = args[0];
+
+//checks if specific file exist/ if not make file
+const jsonPath = path.resolve(__dirname, './tasks.json');
+
+fs.stat(jsonPath, (err, stats) => {
+    if(err){
+      fs.writeFileSync(jsonPath, '')
+    }
+})
 
 switch(firstArgument){
   case 'add':
