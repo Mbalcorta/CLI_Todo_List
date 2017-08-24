736be098e886ea05a5ff12f4a81f96fdd83a2b5e
Mateo Balcorta <misterrattail@gmail.com>
2017-08-24 12:04:31 -0700

    added function skeleton and mocha and chai

--- app/tasks.js
+++ app/tasks.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-const sayHello = () => {
-  return "Hello World"
-};
-
-module.exports = {sayHello};
+const add = require('./commands/add.js').add;
+const complete = require('./commands/complete.js').complete;
+const deleted = require('./commands/delete.js').deleted;
+const list = require('./commands/list.js').list;
+module.exports = {add, complete, deleted, list}
--- test/tasks_test.js
+++ test/tasks_test.js
@@ -1,8 +1,29 @@
 const assert = require('chai').assert;
-const {sayHello} = require('../app/tasks.js');
+const {add, complete, deleted, list} = require('../app/tasks.js');
 
-describe('prints hello',() => {
-  it('should return hello world string', () => {
-    assert.equal(sayHello(), 'Hello World', 'says hello world')
-  })
-})
+
+describe('prints add is exported',() => {
+  it('should return add is exported string', () => {
+    assert.equal(add(), 'add is exported', 'says add is exported');
+  });
+});
+
+
+describe('prints complete is exported',() => {
+  it('should return complete is exported string', () => {
+    assert.equal(complete(), 'complete is exported', 'says complete is exported');
+  });
+});
+
+describe('prints deleted is exported',() => {
+  it('should return deleted is exported string', () => {
+    assert.equal(deleted(), 'deleted is exported', 'says deleted is exported');
+  });
+});
+
+
+describe('prints list is exported',() => {
+  it('should return list is exported string', () => {
+    assert.equal(list(), 'list is exported', 'says list is exported');
+  });
+});
