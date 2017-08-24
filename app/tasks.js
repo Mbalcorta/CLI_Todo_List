96ebbd4ecb43ffe870b0a32be1c4764fd41ce2fa
Mateo Balcorta <misterrattail@gmail.com>
2017-08-24 12:25:31 -0700

    added sub-commands

--- app/tasks.js
+++ app/tasks.js
@@ -3,4 +3,24 @@ const add = require('./commands/add.js').add;
 const complete = require('./commands/complete.js').complete;
 const deleted = require('./commands/delete.js').deleted;
 const list = require('./commands/list.js').list;
+const args = process.argv.slice(2);
+const firstArgument = args[0];
+
+switch(firstArgument){
+  case 'add':
+    add();
+    break;
+  case 'complete':
+    complete();
+    break;
+  case 'delete':
+    deleted();
+    break;
+  case 'list':
+    list();
+    break;
+  default:
+    console.log('Error: not a correct command, please try again')
+}
+
 module.exports = {add, complete, deleted, list}
