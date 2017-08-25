bff672b48feaece6bfff614da6c3820c027dd663
Mateo Balcorta <misterrattail@gmail.com>
2017-08-24 21:05:10 -0700

    added header function

--- app/tasks.js
+++ app/tasks.js
@@ -7,6 +7,7 @@ const deleted = require('./commands/delete.js').deleted;
 const list = require('./commands/list.js').list;
 const args = process.argv.slice(2);
 const firstArgument = args[0];
+const taskString = process.argv[3]
 
 //checks if specific file exist/ if not make file
 const jsonPath = path.resolve(__dirname, './tasks.json');
@@ -15,11 +16,21 @@ fs.stat(jsonPath, (err, stats) => {
     if(err){
       fs.writeFileSync(jsonPath, '')
     }
-})
+});
+
+//need to figure header out
+const header = () => {
+  console.log('ID Description');
+  console.log('-- -------------');
+};
+
+if(taskString){
+  header();
+}
 
 switch(firstArgument){
   case 'add':
-    add();
+    add(taskString)
     break;
   case 'complete':
     complete();
@@ -34,4 +45,4 @@ switch(firstArgument){
     console.log('Error: not a correct command, please try again')
 }
 
-module.exports = {add, complete, deleted, list}
+module.exports = {add, complete, deleted, list, jsonPath, path}
