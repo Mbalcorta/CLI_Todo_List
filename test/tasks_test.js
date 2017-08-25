3d25d2d9775963269f64fb10b3a83fc91dd9b059
Mateo Balcorta <misterrattail@gmail.com>
2017-08-24 21:04:34 -0700

    added test for add function

--- test/tasks_test.js
+++ test/tasks_test.js
@@ -1,29 +1,93 @@
 const assert = require('chai').assert;
-const {add, complete, deleted, list} = require('../app/tasks.js');
+const fs = require('fs');
+const {add, complete, deleted, list, path} = require('../app/tasks.js');
+const terminalText =
+path.resolve( __dirname,'./terminal_print.txt');
+const jsonFile = path.resolve( __dirname,'./test_json.json');
 
 
-describe('prints add is exported',() => {
-  it('should return add is exported string', () => {
-    assert.equal(add(), 'add is exported', 'says add is exported');
+describe('Should add tasks',() => {
+  describe('When once task is added', ()=> {
+    let fileContent;
+    before(()=>{
+      fs.writeFileSync(jsonFile, '{"tasks": [{"id":1,"description":"Buy Milk","incomplete":true}]}');
+      add('Buy Milk');
+      fileContent = fs.readFileSync(terminalText, 'utf8');
+    });
+    it('should print "Created task 1"', () => {
+      assert.equal(fileContent, 'Created task 1');
+    });
   });
+  describe('When two task are added', ()=> {
+    let fileContent;
+    before(()=>{
+      fs.writeFileSync(jsonFile, '{"tasks":[{"id":1,"description":"Buy Milk","incomplete":true},{"id":2,"description":"Buy eggs","incomplete":true}]}');
+      add('Buy eggs');
+      fileContent = fs.readFileSync(terminalText, 'utf8')
+    });
+    it('should print "Created task 2"', () => {
+      assert.equal(fileContent, 'Created task 2');
+    });
+  })
 });
+//
+// describe('prints complete is exported',() => {
+//   it('should return complete is exported string', () => {
+//     assert.equal(complete(), 'complete is exported', 'says complete is exported');
+//   });
+// });
+//
+// describe('prints deleted is exported',() => {
+//   it('should return deleted is exported string', () => {
+//     assert.equal(deleted(), 'deleted is exported', 'says deleted is exported');
+//   });
+// });
 
 
-describe('prints complete is exported',() => {
-  it('should return complete is exported string', () => {
-    assert.equal(complete(), 'complete is exported', 'says complete is exported');
+describe('should return list of incomplete tasks',() => {
+
+  describe('should return no list if no list in task list', () => {
+    let fileContents;
+    before(()=> {
+      fs.writeFileSync(jsonFile, '');
+      fs.writeFileSync(terminalText,'');
+      list();
+      fileContents =  fs.readFileSync(terminalText, 'utf8');
+    });
+
+    after(()=>{
+      fs.writeFileSync(jsonFile, '');
+      fs.writeFileSync(terminalText,'');
+    })
+    //checking before and after of print txt file
+    it('should print "You have 0 tasks" if no task', () => {
+        assert.equal(fileContents, 'You have 0 tasks');
+    });
   });
-});
 
-describe('prints deleted is exported',() => {
-  it('should return deleted is exported string', () => {
-    assert.equal(deleted(), 'deleted is exported', 'says deleted is exported');
-  });
-});
+  describe('should return task object', () => {
+    let fileContents;
+    const jsonTask = '{"tasks":[{"id":1,"description":"Buy Milk","incomplete":true}]}';
+    before(()=> {
+      fs.writeFileSync(jsonFile, jsonTask);
+      fs.writeFileSync(terminalText,'');
+      list();
+      fileContents =  fs.readFileSync(terminalText, 'utf8');
+    });
 
+    after(()=>{
+      fs.writeFileSync(jsonFile, '');
+      fs.writeFileSync(terminalText,'');
+    });
 
-describe('prints list is exported',() => {
-  it('should return list is exported string', () => {
-    assert.equal(list(), 'list is exported', 'says list is exported');
-  });
+    //checking before and after of print txt file
+    it('If one task is added should print one task if incomplete', () => {
+        assert.equal(fileContents, '{"id":1,"description":"Buy Milk","incomplete":true}');
+      });
+  })
+
+  //works with add task// build add task first then come back
+  // describe('should print all tasks that are not complete ', () => {
+  //
+  // })
 });
